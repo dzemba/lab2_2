e8cf1e128a81a68aa160933cb2c4af70407720b8
Radal Dzierzbinski 180157 <Dzemba@op.pl>
2015-04-27 10:34:58 +0200

    Dodanie testu sprawdzającaego dla klasy Money który sprawdza czy dodawanie wartosci da oczekiwany wynik

--- src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -15,4 +15,13 @@ public class MoneyTest {
 			assertThat(t1.multiplyBy(10),is(equalTo(expected)));
 		}
 
+		@Test
+		public void test_add_value_to_other_equel_expected_value() {
+			Money t1 = new Money(20,"PLN");
+			Money t2 = new Money(70,"PLN");
+			Money expected = new Money(90,"PLN");
+			
+			assertThat(t1.add(t2),is(equalTo(expected)));
+		}
+		
 }
